c9550ecf4494731b87de5a29a23f80b4fd0fe22e
Jordan_M_Wofford <jmwofford@gmail.com>
2018-12-05 08:51:04 -0600

    created routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  get '/' => 'users#index'
+  # post '/sessions' => 'sessions#create'
+  # post '/userss' => 'users#create'
+  # get '/dashboard' => 'vm_module#index'
+
 end
