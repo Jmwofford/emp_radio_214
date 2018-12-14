d7a014a8d3fd2b1d7ace8b718e49f3165fbec045
Jordan_M_Wofford <jmwofford@gmail.com>
2018-12-14 13:38:50 -0600

    fix bad repo

--- app/controllers/vm_module_controller.rb
+++ app/controllers/vm_module_controller.rb
@@ -1,5 +1,24 @@
 class VmModuleController < ApplicationController
-  def index 
 
+  before_action :authenticate
+
+  def index 
+  end
+
+  def create 
+   @perform = VmRequest.perform 
+  end
+
+  def update
+    @perform = VmRequest.perform 
+    redirect_to vm_module_path
+  end
+  private 
+
+  def authenticate
+    current_user 
+    unless @current_user
+      redirect_to new_session_path
+    end
   end
 end
